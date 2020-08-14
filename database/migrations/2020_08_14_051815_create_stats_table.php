3aa7fe8e9192c109c68ca11eb8810d5e08732a02
Bewpabo <btf.pabo@gmail.com>
2020-08-14 16:19:19 +0700

    database config add migration

---
+++ database/migrations/2020_08_14_051815_create_stats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('country_id');
+            $table->integer('confirmed')->default(0);
+            $table->integer('deaths')->default(0);
+            $table->integer('recovered')->default(0);
+            $table->integer('active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stats');
+    }
+}
